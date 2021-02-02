2929dfde4b2cfa1734a24dc9cbc9d9031ada04f8
trojo <trojo@DESKTOP-A9UKK45>
2021-02-02 23:59:16 +0100

    Extended Contact and added an equals method

--- contact-book/src/main/java/se/yrgo/models/CompareContact.java
+++ contact-book/src/main/java/se/yrgo/models/CompareContact.java
@@ -1,5 +1,31 @@
 package se.yrgo.models;
 
-public class CompareContact {
-	
+public class CompareContact extends Contact{
+	public CompareContact(String name, String lastName, String address, String phoneNumber, String eMail) {
+		super(name, lastName, address, phoneNumber, eMail);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(this == obj))
+            return false;        
+        Contact other = (Contact) obj;
+        
+        if (getName() == null) {
+            if (other.getName() != null)
+                return false;
+        } else if (!getName().equals(other.getName()))
+            return false;
+        
+        if (getPhoneNumber() == null) {
+            if (other.getPhoneNumber() != null)
+                return false;
+        } else if (!getPhoneNumber().equals(other.getPhoneNumber()))
+            return false;
+        
+       return true;       
+	}
 }
