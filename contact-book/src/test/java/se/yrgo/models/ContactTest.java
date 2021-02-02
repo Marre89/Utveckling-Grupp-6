12659a105f91f6a5396d675709104f96406203ce
Aiste PK <p.aiste@ymail.com>
2021-02-02 22:21:26 +0100

    Adding tests to check two equal and two non equal contacts

--- contact-book/src/test/java/se/yrgo/models/ContactTest.java
+++ contact-book/src/test/java/se/yrgo/models/ContactTest.java
@@ -6,9 +6,18 @@ import org.junit.jupiter.api.Test;
 
 class ContactTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void test2EqualContacts() {
+        Contact contact1 = new Contact("John", "Smith", "Jacobs gata 22", "072-22 49 16", "john@email.com");
+        Contact contact2 = new Contact("John", "Smith", "Jacobs gata 22", "072-22 49 16", "john@email.com");
+        assertEquals(contact1, contact2);
+    }
+
+    @Test
+    public void test2NotEqualContacts() {
+        Contact contact1 = new Contact("John", "Smith", "Jacobs gata 22", "072-22 49 16", "john@email.com");
+        Contact contact2 = new Contact("Andrea", "Johnson", "Lindblads 4", "072-67 98 23", "andrea@email.com");
+        assertNotEquals(contact1, contact2);
+    }
 
 }
