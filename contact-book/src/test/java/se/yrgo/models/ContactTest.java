b2a2cca80ff8144243babe4031a966d764c3a84e
Marre89 <git config --global user.email>
2021-02-02 21:33:04 +0100

    Added ContactTest

---
+++ contact-book/src/test/java/se/yrgo/models/ContactTest.java
@@ -0,0 +1,14 @@
+package se.yrgo.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContactTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
