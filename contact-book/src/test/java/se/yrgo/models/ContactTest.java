5f20d5a2da9e981cb86df05b85ac074c79b6abab
Aiste PK <p.aiste@ymail.com>
2021-02-03 15:54:29 +0100

    Adding tests for every contact property

--- contact-book/src/test/java/se/yrgo/models/ContactTest.java
+++ contact-book/src/test/java/se/yrgo/models/ContactTest.java
@@ -19,5 +19,38 @@ class ContactTest {
         Contact contact2 = new Contact("Andrea", "Johnson", "Lindblads 4", "072-67 98 23", "andrea@email.com");
         assertNotEquals(contact1, contact2);
     }
+    
+    /**
+     * @param These tests check different properties of a Contact class
+     * */
+	@Test
+	public void testContactemail() {
+		Contact email = new Contact("","","","","andrea@email.com");
+		assertEquals("andrea@email.com", email.geteMail());
+	}
+	
+	@Test
+	public void testContactPhoneNumber() {
+		Contact phoneNumber = new Contact("","","","072-22 49 16", "");
+		assertEquals("072-22 49 16", phoneNumber.getPhoneNumber());
+	}
+	
+	@Test
+	public void testContactAddress() {
+		Contact address = new Contact("","","Jacobs gata", "", "");
+		assertEquals("Jacobs gata", address.getAddress());
+	}
+	
+	@Test
+	public void testContactLastName() {
+		Contact johnson = new Contact("andrea", "johnson", "", "", "");
+		assertEquals("johnson", johnson.getLastName());
+	}
+	
+	@Test
+    public void testContactName() {
+        Contact andrea = new Contact("andrea", "andrea@mail.com", "", "", "");
+        assertEquals("andrea", andrea.getName());
+    }
 
 }
