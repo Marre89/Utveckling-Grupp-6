c0e49fd20077272d87aad46fb07036206e3815de
trojandersen <trojandersen@gmail.com>
2021-02-03 20:32:13 +0100

    Comment added, remade equals() method. added tests.

--- contact-book/src/main/java/se/yrgo/models/CompareContact.java
+++ contact-book/src/main/java/se/yrgo/models/CompareContact.java
@@ -10,43 +10,46 @@ public abstract class CompareContact {
 		this.lastName = lastName;
 		this.eMail = eMail;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-	    int result = 1;
-	    result = prime * result + ((name == null) ? 0 : name.hashCode());
-	    result = prime * result + ((lastName == null) ? 0 : name.hashCode());
-	    result = prime * result + ((eMail == null) ? 0 : eMail.hashCode());
-	    return result;
+		int result = 1;
+		result = prime * result + ((eMail == null) ? 0 : eMail.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
 	}
 
 	@Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-        	return true;
-        if (!(this == obj))
-            return false;        
-        Contact other = (Contact) obj;
-        
-        if (name == null) {
-            if (other.getName() != null)
-                return false;
-        } else if (!name.equals(other.getName()))
-            return false;
-        
-        if (lastName == null) {
-            if (other.getLastName() != null)
-                return false;
-        } else if (lastName.equals(other.getLastName()))
-            return false;
-        
-        if (eMail == null) {
-            if (other.geteMail() != null)
-                return false;
-        } else if (!eMail.equals(other.geteMail()))
-            return false;
-        
-       return true;       
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompareContact other = (CompareContact) obj;
+		if (eMail == null) {
+			if (other.eMail != null)
+				return false;
+		} else if (!eMail.equals(other.eMail))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
+	
+	/*This hashcode() & equals() method only checks for the first name, last name and the email. 
+	//More should not be necessary as that would imply a faulty database.
+	*/
+
 }
--- contact-book/src/test/java/se/yrgo/models/CompareContactTest.java
+++ contact-book/src/test/java/se/yrgo/models/CompareContactTest.java
@@ -1,19 +1,28 @@
 package se.yrgo.models;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CompareContactTest {
 
 	@Test
-	void CompareContactNullTest() {
+	public void CompareContactNullTest() {
 		assertFalse(equals(null));
 	}
 
-
 	@Test
-	public void tester() {
-		//todo
+	public void CompareEquals() {
+		Contact contact1 = new Contact("John", "Smith", "Jacobs gata 22", "072-22 49 16", "john@email.com");
+		Contact contact2 = new Contact("John", "Smith", "Jacobs gata 22", "072-22 49 16", "john@email.com");
+        assertEquals(contact1, contact2);
 	}
+	
+	@Test
+	public void CompareNotEquals() {
+		Contact contact3 = new Contact("Bobby", "Wright", "Street 22", "072-11 49 16", "bob@email.com");
+		Contact contact4 = new Contact("Bobby", "Write", "Street 22", "072-11 49 16", "bob@email.com");
+        assertNotEquals(contact3, contact4);
+	}	
+	
 }
